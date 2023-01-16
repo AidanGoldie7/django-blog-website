c39e8e722379621a8e75a10d79ead0dc6d4e9b8a
aidangoldie7 <40402670@live.napier.ac.uk>
2023-01-16 15:48:46 +0000

    7. add static file + settings

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -3,11 +3,12 @@ from blog.models import Post,Comment
 
 
 class PostForm(forms.ModelForm):
-    
+    #set up fields for form 
     class Meta():
         model = Post 
         fields = ('author', 'title', 'text')
 
+        #attribute css classes to widgets
         widgets ={
             'title':forms.TextInput(attrs={'class':'textinputclass'}),
             'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
@@ -18,11 +19,12 @@ class PostForm(forms.ModelForm):
 
 
 class CommentForm(forms.ModelForm):
-
+    #set up fields for form
     class Meta():
         model = Comment 
         fields = ('author', 'text')
 
+        #attribute css classes to widgets
         widgets ={
             'author':forms.TextInput(attrs={'class':'textinputclass'}),
             'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
