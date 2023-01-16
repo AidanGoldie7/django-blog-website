c5ded942e5b7b535da800c3d394cb27ce2baff08
aidangoldie7 <40402670@live.napier.ac.uk>
2023-01-16 12:25:23 +0000

    5. models.py - create Post and Comment classes

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+
 
 # Create your models here.
+class Post(models.Model):
+    #setting up values
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    #method for publishing a post and giving it timestamp 
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    #method to approve comments, 'comments' will be made later on
+    #to store user comments on a post 
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    #has to be called get_absolute_url as this is what django looks 
+    #for by default
+    def get_absolute_url(self):
+        #once post is created, go to the details page of the post
+        return reverse("post_detail",kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+
+
+class Comment(models.Model):
+    #setting up values
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length = 200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True 
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text 
